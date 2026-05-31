a4b4b498684ac3d2a9d3f40404a952cfa064c4ba
deji12 <adesolaayodeji53@gmail.com>
2026-05-31 14:34:02 +0100

    setup log endpoint

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from datetime import datetime
+from django.http import JsonResponse
 
-# Create your views here.
+def logs(request):
+
+    try:
+        log_file = f'bot/data/logs/growthbot_{datetime.now().strftime("%Y-%m-%d")}.log'
+        with open(log_file, 'r') as file:
+            logs = file.read()
+    except FileNotFoundError:
+        logs = ''
+
+    return JsonResponse({
+        'logs': logs,
+    })
+
